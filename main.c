23c7a4b639f4ad886aa77584f46df35d4135c3b7
ifurdey2 <ifurdey2@gmail.com>
2019-12-04 15:59:16 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MIP GIT
+//
+//  Created by Ilia Furdey on 12/4/19.
+//  Copyright © 2019 Ilia Furdey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
